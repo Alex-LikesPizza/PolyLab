05a1e5da93717180181898ad282d1dfebddacb4b
Alex-LikesPizza <alexandru13321@gmail.com>
2024-06-10 19:21:23 +0300

    web page setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "./components/layout/header/Header.jsx";
+import Footer from "./components/layout/footer/Footer.jsx";
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <div>App</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../../images/vite.svg"
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+
+    </header>
+  )
+}
+
+export default Header
